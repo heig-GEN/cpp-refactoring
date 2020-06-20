65e7910983feba4ca32f97d42258275916788d1e
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-20 14:52:13 +0200

    Refactoring : extract getFrequentRenterPoints

--- src/Rental.h
+++ src/Rental.h
@@ -10,6 +10,8 @@ public:
 
     int getDaysRented() const;
 
+    int getFrequentRenterPoints() const;
+
     const Movie& getMovie() const;
 
 private:
@@ -21,6 +23,13 @@ inline Rental::Rental(const Movie& movie, int daysRented)
         : _movie(movie), _daysRented(daysRented) {}
 
 inline int Rental::getDaysRented() const { return _daysRented; }
+
+inline int Rental::getFrequentRenterPoints() const {
+    if (getMovie().getPriceCode() == Movie::NEW_RELEASE && getDaysRented() > 1)
+        return 2;
+    else
+        return 1;
+}
 
 inline const Movie& Rental::getMovie() const { return _movie; }
 
