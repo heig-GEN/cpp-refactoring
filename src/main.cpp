fb90a5fe58d9e7e5e1f8f15bc2e5979c64efa3df
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-20 15:51:15 +0200

    Reformat code

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,15 @@
 int main() {
 
     Customer customer("Olivier");
-    customer.addRental( Rental( Movie("Karate Kid"), 7));
-    customer.addRental( Rental( Movie( "Avengers: Endgame", &NewRelease::INSTANCE ), 5));
-    customer.addRental( Rental(Movie("Snow White", &Children::INSTANCE), 3 ));
+    customer.addRental(Rental(
+            Movie("Karate Kid"), 7)
+    );
+    customer.addRental(Rental(
+            Movie("Avengers: Endgame", &NewRelease::INSTANCE), 5)
+    );
+    customer.addRental(Rental(
+            Movie("Snow White", &Children::INSTANCE), 3)
+    );
 
     std::cout << customer.statement() << std::endl;
 
