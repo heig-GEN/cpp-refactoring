5e85471dcf660ec02d8d5632ee85598109c7267a
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-20 17:46:15 +0200

    Add Regular getPrice test

--- test/test_categories.cpp
+++ test/test_categories.cpp
@@ -17,3 +17,12 @@ TEST(categories, newReleaseGetPriceIsCorrect) {
     EXPECT_EQ(6, category.getPrice(2));
     EXPECT_EQ(9, category.getPrice(3));
 }
+
+TEST(categories, regularGetPriceIsCorrect) {
+    Category category = Regular::INSTANCE;
+    EXPECT_EQ(2, category.getPrice(0));
+    EXPECT_EQ(2, category.getPrice(1));
+    EXPECT_EQ(2, category.getPrice(2));
+    EXPECT_EQ(3.5, category.getPrice(3));
+    EXPECT_EQ(5, category.getPrice(4));
+}
