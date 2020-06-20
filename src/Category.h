e7098c0d37de6e08a016496da4119f8ef6ac8a7b
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-20 15:14:24 +0200

    Add some Category children

--- src/Category.h
+++ src/Category.h
@@ -2,5 +2,33 @@
 #ifndef CATEGORY_H
 #define CATEGORY_H
 
+class Category {
+public:
+    virtual double getPrice(unsigned days) const = 0;
+};
+
+class Children : public Category {
+    double getPrice(unsigned int days) const override {
+        double price = 1.5;
+        if (days > 3)
+            price += (days - 3) * 1.5;
+        return price;
+    }
+};
+
+class NewRelease : public Category {
+    double getPrice(unsigned int days) const override {
+        return days * 3;
+    }
+};
+
+class Regular : public Category {
+    double getPrice(unsigned int days) const override {
+        double price = 2;
+        if (days > 2)
+            price += (days - 2) * 1.5;
+        return price;
+    }
+};
 
 #endif // CATEGORY_H
