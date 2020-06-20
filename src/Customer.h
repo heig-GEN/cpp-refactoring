a45bccdadf60e608f718bd358e5480546a237cd5
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-20 14:45:34 +0200

    Reformat code

--- src/Customer.h
+++ src/Customer.h
@@ -1,6 +1,7 @@
 // Customer.h
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
+
 #include <string>
 #include <vector>
 #include "Rental.h"
@@ -8,28 +9,31 @@
 class Customer {
 public:
     Customer();
-    explicit Customer( const std::string& name );
 
-    void addRental( const Rental& arg );
+    explicit Customer(const std::string& name);
+
+    void addRental(const Rental& arg);
+
     std::string getName() const;
+
     std::string statement();
 
 private:
     std::string _name;
-    std::vector< Rental > _rentals;
+    std::vector<Rental> _rentals;
 };
 
-inline Customer::
-Customer() {}
+inline Customer::Customer() {}
 
-inline Customer::
-Customer( const std::string& name )
-        : _name( name ) {}
+inline Customer::Customer(const std::string& name)
+        : _name(name) {}
 
-inline void Customer::
-addRental( const Rental& arg ) { _rentals.push_back( arg ); }
+inline void Customer::addRental(const Rental& arg) {
+    _rentals.push_back(arg);
+}
 
-inline std::string Customer::
-getName() const { return _name; }
+inline std::string Customer::getName() const {
+    return _name;
+}
 
 #endif // CUSTOMER_H
--- src/Rental.h
+++ src/Rental.h
@@ -1,13 +1,15 @@
 // Rental.h
 #ifndef RENTAL_H
 #define RENTAL_H
+
 #include "Movie.h"
 
 class Rental {
 public:
-    Rental( const Movie& movie, int daysRented );
+    Rental(const Movie& movie, int daysRented);
 
     int getDaysRented() const;
+
     const Movie& getMovie() const;
 
 private:
@@ -15,15 +17,11 @@ private:
     int _daysRented;
 };
 
-inline Rental::
-Rental( const Movie& movie, int daysRented )
-        : _movie( movie )
-        , _daysRented( daysRented ) {}
+inline Rental::Rental(const Movie& movie, int daysRented)
+        : _movie(movie), _daysRented(daysRented) {}
 
-inline int Rental::
-getDaysRented() const { return _daysRented; }
+inline int Rental::getDaysRented() const { return _daysRented; }
 
-inline const Movie& Rental::
-getMovie() const { return _movie; }
+inline const Movie& Rental::getMovie() const { return _movie; }
 
 #endif // RENTAL_H
