cc7d232e4994c2a0a629c23afc131687053d1c85
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-21 16:58:41 +0200

    Add movie setCategory tests

--- test/test_movies.cpp
+++ test/test_movies.cpp
@@ -34,3 +34,21 @@ TEST(movies, getFrequentRenterPointsIsCorrect) {
     Movie movie("A", &category);
     EXPECT_EQ(movie.getFrequentRenterPoints(1), category.getFrequentRenterPoints(1));
 }
+
+TEST(movies, setCategoryIsCorrect) {
+    MockCategory cat1;
+    MockCategory cat2;
+
+    EXPECT_CALL(cat1, getFrequentRenterPoints)
+            .Times(AtLeast(2))
+            .WillRepeatedly(Return(10));
+
+    EXPECT_CALL(cat2, getFrequentRenterPoints)
+            .Times(AtLeast(2))
+            .WillRepeatedly(Return(5));
+
+    Movie movie("A", &cat1);
+    EXPECT_EQ(movie.getFrequentRenterPoints(1), cat1.getFrequentRenterPoints(1));
+    movie.setCategory(&cat2);
+    EXPECT_EQ(movie.getFrequentRenterPoints(1), cat2.getFrequentRenterPoints(1));
+}
